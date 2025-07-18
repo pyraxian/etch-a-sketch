c55942c8f98e7c27515dd1eaab3e2e6a5f0ed678
Connor Neill <cwmn1975@comcast.net>
2025-07-18 16:23:18 -0400

    Add mouseover listener
    Added a mouseover listener to the grid divs, changing their color when they are moused over.

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ function createGridDivs(num) {
     for (let i = 0; i < (num * num); i++) {
         const newDiv = document.createElement("div");
         newDiv.setAttribute("class", "grid");
+        newDiv.setAttribute('id', `box${i}`);
         masterContainer.appendChild(newDiv);
     }
 }
 
 createGridDivs(16);
+
+// Mouseover listener
+const masterContainer = document.querySelector("#masterContainer");
+
+masterContainer.addEventListener('mouseover', (event) => {
+    console.log(`Box ${event.target.id} was moused over`);
+    event.target.style.background = 'blue';
+});
